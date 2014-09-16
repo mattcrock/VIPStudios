860f6fc886494e1911b8811fd2657b09ce163c4f
Matthew Crockett <matthew@synthesis.co.za>
2014-09-16 08:51:44 +0200

    adding the home page get.

--- CycleOps/routes/index.js
+++ CycleOps/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', {title:'Home', currentURL:'/index/'})
+})
+
 router.get('/index', function(req, res) {
   res.render('index', { title:'Home', currentURL:'/index/'}
    );
